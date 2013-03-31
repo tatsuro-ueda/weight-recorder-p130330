abcf3d9ce7813772d343a71046a2b27f39f89cc6
Tatsuro Ueda <ueda_tatsuro@yahoo.co.jp>
2013-03-31 12:49:39 +0900

    sign upしたらlog in画面が表示されるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to records_url, :notice => "Signed up!"
+      redirect_to log_in_url, :notice => "Signed up!"
     else
       render "new"
     end
